86f01d352b30102722c3cc338d13b7f994112b89
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2023-06-13 11:07:47 -0300

    Delete user

--- backend/wano/app/Http/Controllers/UserController.php
+++ backend/wano/app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\User;
 
 class UserController extends Controller
@@ -36,6 +37,21 @@ class UserController extends Controller
     public function show($id)
     {
         return response()->json(User::with(['role'])->where('id', '=', $id)->get());
+    }
+
+    /**
+     * Delete the specified resource.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+        $user = User::findOrFail($id);
+        DB::table('user_roles')->where('user_id', '=', $id)->update(['deleted_at' => now()]);
+        $user->delete();
+
+        return response()->json(['deletedId' => $user]);
     }
 
 }
--- backend/wano/app/User.php
+++ backend/wano/app/User.php
@@ -5,11 +5,13 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
